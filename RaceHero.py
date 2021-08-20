cd66b2a05ef172c585cda5f729728ef9b59f50eb
rinAkhm <rin_akhm@bk.ru>
2021-08-20 18:50:56 +0300

    race hero

--- RaceHero.py
+++ RaceHero.py
@@ -1,159 +1,135 @@
+"""Абстрактная фабрика по созданию классов героя."""
+
 from abc import ABC, abstractmethod
-from Items import Manager
 import random
+from typing import Any
 
 
 class ObjectRace(ABC):
+    """Абстрактная раса."""
 
     @abstractmethod
-    def get_skill(self):
+    def skill_dodge(self, race: str, enemy: str) -> None:
+        """Улонение от атак."""
         pass
 
     @abstractmethod
-    def choice_race(self):
+    def choice_race(self) -> None:
+        """Выбор расы."""
         pass
 
     @abstractmethod
-    def improve_weapon(self):
+    def skill_improve_weapon(self, race: str, weapon: str) -> None:
+        """Классовые оружия."""
         pass
 
 
 class Warrior(ObjectRace):
-    def get_skill(self, enemy):
+    """Абстрактный класс Warrior."""
+
+    def skill_dodge(self, race: str, enemy: str) -> Any:
+        """Абстрактный метод уклонения от атак."""
         skill = [True, False]
-        if enemy == 'meelle':
+        if enemy == "meelle":
             dodge = random.choice(skill)
             return dodge
         else:
-            return 0
+            return False
 
-    def choice_race(self):
-        return 'Warrior'
+    def choice_race(self) -> str:
+        """Абстрактный метод выбора расы."""
+        return "Warrior"
 
-    def improve_weapon(self, race, weapon):
-        if race == f'{self.choice_race()}' and weapon.get('rapier', False):
-            weapon['rapier'] += 5
-            return weapon
+    def skill_improve_weapon(self, race: str, weapon: str) -> int:
+        """Абстрактный метод классового оружия."""
+        attack = 0
+        if race == f"{self.choice_race()}" and weapon == "rapier":
+            attack += 5
+            return attack
         else:
             return 0
 
 
 class Mage(ObjectRace):
-    def get_skill(self, enemy):
+    """Абстрактный класс Mage."""
+
+    def skill_dodge(self, race: str, enemy: str) -> Any:
+        """Абстрактный метод уклонения от атак."""
         skill = [True, False]
-        if enemy == 'spell_shot':
+        if enemy == "spell_shot":
             dodge = random.choice(skill)
             return dodge
         else:
-            return 0
+            return False
 
-    def choice_race(self):
-        return 'Mage'
+    def choice_race(self) -> str:
+        """Абстрактный метод выбора расы."""
+        return "Mage"
 
-    def improve_weapon(self, race, weapon):
-        if race == f'{self.choice_race()}' and weapon.get('magic_book', False):
-            weapon['magic_book'] += 5
-            return weapon
+    def skill_improve_weapon(self, race: str, weapon: str) -> int:
+        """Абстрактный метод классового оружия."""
+        attack = 0
+        if race == f"{self.choice_race()}" and weapon == "magic_book":
+            attack += 5
+            return attack
         else:
             return 0
 
 
 class Hunter(ObjectRace):
-    def get_skill(self, enemy):
+    """Абстрактный класс Hunter."""
+
+    def skill_dodge(self, race: str, enemy: str) -> Any:
+        """Абстрактный метод уклонения от атак."""
         skill = [True, False]
-        if enemy == 'long_shot':
+        if enemy == "long_shot":
             dodge = random.choice(skill)
             return dodge
+        else:
+            return False
 
-    def choice_race(self):
-        return 'Hunter'
+    def choice_race(self) -> str:
+        """Абстрактный метод выбора расы."""
+        return "Hunter"
 
-    def improve_weapon(self, race, weapon):
-        if race == f'{self.choice_race()}' and weapon == 'bow':
-            weapon += 5
-            return weapon
+    def skill_improve_weapon(self, race: str, weapon: str) -> int:
+        """Абстрактный метод классового оружия."""
+        attack = 0
+        if race == f"{self.choice_race()}" and weapon == "bow":
+            attack += 5
+            return attack
         else:
             return 0
 
 
 class RaceFactory(ABC):
+    """Фабрика по созданию абстрактного класса."""
+
     @abstractmethod
-    def create_race(self):
+    def create_race(self) -> None:
+        """Абстрактный метод создания класса."""
         pass
 
 
 class WarriorFactory(RaceFactory):
-    def create_race(self):
+    """Конкретная фабрика класса Warrior."""
+
+    def create_race(self) -> Warrior:
+        """Метод создания класса."""
         return Warrior()
 
 
 class MageFactory(RaceFactory):
-    def create_race(self):
+    """Конкретная фабрика класса Mage."""
+
+    def create_race(self) -> Mage:
+        """Метод создания класса."""
         return Mage()
 
 
 class HunterFactory(RaceFactory):
-    def create_race(self):
+    """Конкретная фабрика класса Hunter."""
+
+    def create_race(self) -> Hunter:
+        """Метод создания класса."""
         return Hunter()
-
-
-class ManagerRace:
-    def get_race(self, race):
-        spawner_to_factory_mapping = {
-            "Warrior": WarriorFactory,
-            "Mage": MageFactory,
-            "Hunter": HunterFactory
-        }
-
-        spawner = spawner_to_factory_mapping[race]()
-        enemy = spawner.create_enemy()
-        action = enemy.damage()
-        return action
-
-
-# boy = WarriorFactory()
-# action = boy.create_race()
-# print(action.choice_race())
-# action.get_skill('meelle')
-# action.improve_weapon(action.choice_race(), {'rapier': 15})
-#
-# her = Hero(name="Rinat", race=action.choice_race(), weapon={'rarpier': 15})
-# print(her)
-
-#
-# class temp(Hero, ObjectRace):
-#     pass
-#
-#     def get_race(self, race):
-#         self.race = {}
-#         if race == 1:
-#             race_skill = random.randint(1, 10)
-#             race_skill['Warrior'] = race_skill
-#             return race_skill
-#
-#         elif self.race == 2:
-#             race_skill = random.randint(1, 10)
-#             race_skill['Mage'] = race_skill
-#             return race_skill
-#
-#         elif self.race == 3:
-#             race_skill = random.randint(1, 10)
-#             race_skill['Hunter'] = race_skill
-#             return race_skill
-#
-#     def choice_race(self):
-#         while True:
-#             try:
-#                 race = int(input("Выберите расу:\n"
-#                                  "1. Warrior\n"
-#                                  "2. Mage\n"
-#                                  "3. Hunter\n"))
-#             except TypeError:
-#                 raise ('Ошибка ввода, попробуй ввести еще раз')
-#                 continue
-#             if race == 1 or race == 2 or race == 3:
-#                 self.get_race(race)
-#                 return
-#
-#
-# per = Hero()
